1943a6ba0eaa8ee2800d32237baae0c852954bf9
Amir Tadrisi <amirtds@gmail.com>
2024-07-18 18:27:15 -0500

    chore: bump dependencies

--- project/core/settings.py
+++ project/core/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/staticfiles/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
